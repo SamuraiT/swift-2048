72b4a638b21452a733729fb860d9b179b0eb369a
SamuraiT <t.yasukawa01@gmail.com>
2014-12-15 10:49:27 +0900

    Initial Commit

---
+++ 2048/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2048
+//
+//  Created by tatsuro-y on 2014/12/15.
+//  Copyright (c) 2014年 tatsuro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
